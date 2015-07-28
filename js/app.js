81fbe9fa90577a7f7b7ff37e4d6b33a0445287ef
JuanPablo <jpabloaj@gmail.com>
2015-07-27 23:07:25 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var firebaseUrl = 'https://programadores-chile.firebaseio.com/';
+var ref = new Firebase(firebaseUrl);
+var refUsers = ref.child('users');
+
+var app = angular.module('app', ['firebase']);
+
+app
+  .factory('Auth', ['$firebaseAuth', function($firebaseAuth){
+    return($firebaseAuth(ref));
+  }])
+  .controller('UsersController', function($scope, Auth, $firebaseArray){
+    $scope.users = $firebaseArray(refUsers);
+
+    $scope.auth = Auth;
+
+    $scope.auth.$onAuth(function(authData){
+      $scope.authData = authData;
+      if (authData) {
+        var username = authData.github.username;
+        $scope.username = username;
+
+        refUsers.child(username).once('value', function(data){
+          // if user not exist create
+          if(!data.val()){
+            refUsers.child(username).set({
+              username:username
+            });
+          }
+        })
+      }
+    });
+
+  });
